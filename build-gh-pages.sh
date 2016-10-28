e964299d85ad6cd2f59a73429a40614bbd0ea91e
bmsleight <bms(#a#)barwap.com>
2016-10-28 15:52:01 +0100

    Updating how...

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 cd $(mktemp -d)
-git clone https://github.com/bmsleight/delegation-presentation.git pages
-cd pages
-git checkout gh-pages
-rm index.html
-hovercraft delegation.rst ./
+git clone https://github.com/bmsleight/delegation-presentation.git source
+git clone --branch gh-pages https://github.com/bmsleight/delegation-presentation.git gh-pages
+hovercraft ./source/delegation.rst ./gh-pages/
+cd ./gh-pages/
 git add -A
 git commit -m "update docs"
 git push origin gh-pages
